9c452aa980d0043394654fa44cf31a63d0c50f05
TiaDev7474 <nomenjanaharyriry@gmail.com>
2023-07-08 22:02:31 +0300

    fix: fix loader page bug

--- js/animation.js
+++ js/animation.js
@@ -1,10 +1,12 @@
 
 (function(){
     document.querySelector('#loader').classList.add('loader');
+    
 
     document.addEventListener('DOMContentLoaded',()=>{
          setTimeout(() =>{
             document.querySelector('#loader').classList.remove('loader'); 
+            document.querySelector('.overlay').classList.remove('overlay-hide');
          },2000)
     })
 })()
